5e33e4df93157754aa40a9b022f29bd5baf7d078
Scott Barrett <scott_barrett@live.com.au>
2022-02-21 22:58:03 +1100

    Basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './componants/main/main.component';
+import { NavigationComponent } from './componants/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
